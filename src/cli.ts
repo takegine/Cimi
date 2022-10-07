31f963bbb8f01be521ba6fb41807560ad4d97696
西索酱 <takegine@vip.qq.com>
2022-10-07 14:32:49 +0800

    整理完成

--- src/cli.ts
+++ src/cli.ts
@@ -2,113 +2,15 @@
 
 import { program } from 'commander'
 import { version } from '../package.json'
-import { green, cyan, getVersion, writeNewVersion } from '.'
-import { exec } from 'child_process'
-
-const enum changeType {
-  patch = 'patch', 
-  minor = 'minor', 
-  major = 'major',
-}
-
-const msg = console.info;
-const { projectVersion, projectName } = getVersion()
-var state: changeType
-var branch: string
-var [major, minor, patch] = projectVersion
+import qpub from '.'
 
 program
   .name('qpub')
   .version(version, '-v, --version')
-  .on('--help', () => {
-    msg('\n  Tip:\n')
-    msg('    You should run this script in the root directory of you project or run by npm scripts.')
-    msg('\n  Examples:\n')
-    msg(`    ${green('$')} qpub patch [branch] (default: master)`)
-    msg(`    ${green('$')} qpub minor [branch] (default: master)`)
-    msg(`    ${green('$')} qpub major [branch] (default: master)`)
-    msg(`    ${green('$')} qpub patch -B [branch] (default: master)`)
-    msg(`    ${green('$')} qpub minor -B [branch] (default: master)`)
-    msg(`    ${green('$')} qpub major -B [branch] (default: master)`)
-    msg('')
-  })
+  .option('-B --beta', 'publish your new npm package with beta', false)
+  .option('-patch', 'patch your new npm package with beta', true)
+  .option('-minor', 'minor your new npm package with beta', false)
+  .option('-major', 'major your new npm package with beta', false)
+  .option('-branch [master]', 'pull this branch', 'master')
+  .action(qpub)
   .parse(process.argv)
-  .option('-B --beta', 'patch your new npm package with beta', false)
-  .option('patch [branch]', 'patch your new npm package,branch default: master',a=>{
-    state = changeType.patch
-    patch += 1;
-    return a
-  }, 'master')
-  .option('minor [branch]', 'minor your new npm package,branch default: master', a => {
-    state = changeType.minor
-    minor += 1;
-    return a
-  }, 'master')
-  .option('major [branch]', 'major your new npm package,branch default: master', a => {
-    state = changeType.major
-    major += 1;
-    return a
-  }, 'master')
-  .action(async (dir,cmd)=>{
-    msg(state, branch ,dir, cmd)
-    msg(green(`Start to ${state} version to ${projectName}...`))
-    let newVersion = `${+major}.${+minor}.${+patch}${cmd.beta ? '-beta' : ''}`
-    writeNewVersion(newVersion)
-    msg(green(`\nVersion: ${cyan(`${projectVersion} -> ${newVersion}`)}`))
-    msg(green(`${state} ${projectName} version to ${newVersion}`))
-    await execShell(state,newVersion)
-    msg(`\n${green('[ qpub ]')} Release ${projectName} Success!\n`)
-  })
-
-//执行整个流程的命令
-function execShell(type: string, newVersion:string) {
-  const echo1 = [
-    '[ 1 / 3 ]',
-    `Commit and push to ${branch} branch`
-  ]
-  const part1 = [
-    'git add .',
-    `git commit -m "${type} version to ${newVersion}"`,
-    `git push origin ${branch}`,
-  ]
-  const echo2 = [
-    '[ 2 / 3 ]',
-    `Tag and push tag to ${branch}`
-  ]
-
-  const part2 = [
-    `git tag ${newVersion}`,
-    `git push origin ${newVersion}`,
-  ]
-  const echo3 = [
-    '[ 3 / 3 ]',
-    'Publish to NPM'
-  ]
-  const part3 = [
-    `npm publish ${program['accessPublic'] ? '--access=public' : ''}`,
-  ]
-  return step(echo1, part1)
-    .then(() => step(echo2, part2))
-    .then(() => step(echo3, part3))
-}
-
-async function step(desc: string[], command: string[]) {
-  msg(`${green(desc[0])} ${cyan(desc[1])}`)
-  return new Promise((resolve, reject) => {
-    const childExec = exec(
-      command.join(' && '),
-      { maxBuffer: 10000 * 10240 },
-      (err, stdout, stderr) => {
-        console.log(err, stdout, stderr)
-        if (err) {
-          reject(err)
-          throw err
-        } else {
-          resolve('')
-        }
-      }
-    )
-    childExec.stdout?.pipe(process.stdout)
-    childExec.stderr?.pipe(process.stderr)
-  })
-}
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,122 @@
 import { readFileSync, writeFileSync } from 'fs'
 import { resolve } from 'path'
 import chalk from 'chalk'
+import { exec } from 'child_process';
 
-const { cyan, green, red } = chalk
-export { cyan, green, red };
-const _path = resolve(process.cwd(), 'package.json');
-var projectVersion, projectName
-
-export function getVersion(): {
-  projectVersion: [number, number, number],
-  projectName: string
-} {
-  const packageJson = JSON.parse(
-    readFileSync(_path, 'utf8')
-  )
-
-  projectVersion = packageJson.version
-  projectName = packageJson.name
-
-  let [major, minor, patch] = projectVersion.split('.')
-  if (patch.length > 2 && patch.includes('-beta')) {
-    patch = patch.split('-')[0];
-  }
-  return {
-    projectVersion: [major, minor, patch],
-    projectName: packageJson.name,
-  }
+declare interface optionType {
+  Patch: boolean,
+  Minor: boolean,
+  Major: boolean,
+  beta: boolean,
+  Branch: string
 }
 
-//写入新版本号，更新项目文件
-export function writeNewVersion(newVersion: string) {
-  const packageJson = readFileSync(_path, 'utf8')
-  const newPackageJson = packageJson.replace(
-    `"version": "${projectVersion}"`,
-    `"version": "${newVersion}"`
-  )
-  writeFileSync(_path, newPackageJson)
-  console.info(green('\nUpdate package.json success!'))
+const enum changeType {
+  patch = 'patch',
+  minor = 'minor',
+  major = 'major',
 }
 
+const msg = console.info;
+const { cyan, green, red } = chalk;
+const package_path = resolve(process.cwd(), 'package.json');
+
+export default async (options: optionType, cmd:any) => {
+  let state: changeType
+  switch (true) {
+    case options.Major:
+      state = changeType.major
+      break;
+    case options.Minor:
+      state = changeType.minor
+      break;
+    case options.Patch: default:
+      state = changeType.patch
+      break;
+  }
+
+  // msg(options)
+  let branch = options.Branch
+  let packageJson = getPackage()
+  msg(green(`Start to ${state} version to ${packageJson.name}...`))
+
+  try {
+    bar('[ 1 / 4 ]', 'Update package.json')
+    let version = updateVersion(packageJson, state, options.beta)
+    bar('[ 2 / 4 ]', `Commit and push to ${branch} branch`)
+    await step([
+      'git add .',
+      `git commit -m "${state} version to ${version}"`,
+      `git push origin ${branch}`,
+    ])
+    bar('[ 3 / 4 ]', `Tag and push tag to ${branch}`)
+    await step([
+      `git tag ${version}`,
+      `git push origin ${version}`,
+    ])
+    bar('[ 4 / 4 ]', 'Publish to NPM')
+    await step([
+      `npm publish ${cmd['accessPublic'] ? '--access=public' : ''}`,
+    ])
+  } catch (error) {
+    msg(`\n${red('[ qpub ]')} Release ${packageJson.name} Fail!\n`)
+    console.error(error)
+    process.exit(1);
+  }
+
+  msg(`\n${red('[ qpub ]')} Release ${packageJson.name} Success!\n`)
+}
+
+function getPackage() {
+  let packageStr = readFileSync(package_path, 'utf8')
+  return JSON.parse(packageStr)
+}
+
+function updateVersion(packageJson:any,state:changeType, beta:boolean) {
+  let { version: oldVer } = packageJson;
+
+  let [major, minor, patch ] = oldVer.split(/[\-|\.]/g)
+  let newVer:string
+  switch (state) {
+    case changeType.major:
+      newVer = `${+major+1}.0.0`
+      break;
+    case changeType.minor:
+      newVer = `${+major}.${+minor+1}.0`
+      break;
+    case changeType.patch: default:
+      newVer = `${+major}.${+minor}.${+patch + 1}`
+      break;
+  }
+  if(beta)
+    newVer += '-beta'
+
+  packageJson.version = newVer
+
+  bar('\nVersion: ', `${oldVer} -> ${newVer}`)
+  writeFileSync(package_path, JSON.stringify(packageJson, null, '  '))
+  // msg(green('\nUpdate package.json success!'))
+  return newVer
+}
+
+async function step(commands: string[]) {
+  return new Promise((resolve, reject) => {
+    const childExec = exec(
+      commands.join(' && '),
+      { maxBuffer: 10000 * 10240 },
+      (err, stdout, stderr) => {
+        msg(err, stdout, stderr)
+        if (err)
+          reject(err) 
+        else
+          resolve('')
+      }
+    )
+    childExec.stdout?.pipe(process.stdout)
+    childExec.stderr?.pipe(process.stderr)
+  })
+}
+
+function bar(Progress:string, desc:string) {
+  msg(`  ${green(Progress)} ${cyan(desc)}`)
+}
